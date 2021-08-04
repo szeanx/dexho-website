0cb76a39fe8e3a32e18e0c6197e352ced4787ecf
sean <tp063366@mail.apu.edu.my>
2021-08-04 16:46:19 +0800

    added barba.js and page animations

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-// this is the nav bar section
+// this is the nav bar section, toggling css classes on mobile menu / navbar
 $("#toggle").click(function () {
  $(this).toggleClass("on");
  $("#resize").toggleClass("active");
@@ -12,6 +12,7 @@ $(".close-btn").click(function () {
  $("#resize").toggleClass("active");
 });
 
+// changes the color of nav bar when scrolled dependent on how far the user has scrolled
 $(function () {
  $(document).scroll(function () {
   var $nav = $(".nav");
@@ -60,3 +61,72 @@ $('a[href*="#"]')
    }
   }
  });
+
+//  page transitions
+
+// delay function
+function delay(n) {
+ n = n || 2000;
+ return new Promise((done) => {
+  setTimeout(() => {
+   done();
+  }, n);
+ });
+}
+
+function pageTransition() {
+ var tl = gsap.timeline();
+ tl.to(".loading-screen", {
+  duration: 1.2,
+  width: "100%",
+  left: "0%",
+  ease: "Expo.easeInOut",
+ });
+
+ tl.to(".loading-screen", {
+  duration: 1,
+  width: "100%",
+  left: "100%",
+  ease: "Expo.easeInOut",
+  delay: 0.3,
+ });
+
+ tl.set(".loading-screen", { left: "-100%" });
+}
+
+function contentAnimation() {
+ var tl = gsap.timeline();
+ tl.from(".animate-this", {
+  duration: 1,
+  y: 30,
+  opacity: 0,
+  stagger: 0.4,
+  delay: 0.2,
+ });
+}
+
+$(function () {
+ barba.init({
+  sync: true,
+
+  transitions: [
+   {
+    async leave(data) {
+     const done = this.async();
+
+     pageTransition();
+     await delay(1000);
+     done();
+    },
+
+    async enter(data) {
+     contentAnimation();
+    },
+
+    async once(data) {
+     contentAnimation();
+    },
+   },
+  ],
+ });
+});
